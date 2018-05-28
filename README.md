6c21f889d14d201400cd0556e7184be972827db1
yinglan <sufly0001@gmail.com>
2018-05-28 14:59:19 +0800

    增加部分方法

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 ## Abstract 摘要
   自用工具类 常用函数式
 
+### 1.0.3 TAG
+增加部分功能，删除部分代码
+
+compile 'com.yinglan.common:common:1.0.3'
 
 ### 1.0.2 TAG
 增加部分功能，删除部分代码
-
-compile 'com.yinglan.common:common:1.0.2'
 
 ### 1.0.1 TAG
 
--- common/src/main/java/com/yinglan/common/DateUtils.java
+++ common/src/main/java/com/yinglan/common/DateUtils.java
@@ -8,6 +8,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 
 public class DateUtils {
@@ -15,45 +16,36 @@ public class DateUtils {
     /**
      * 指定格式返回当前系统时间
      *
-     * @param format String 想要格式化的日期形式,如"yyyy年MM月dd日 hh:mm"
+     * @param format String 想要格式化的日期形式,如"yyyy年MM月dd日 HH:mm"
      * @return 指定格式返回当前系统时间, 如"XXXX年XX月XX日 XX:XX"
      */
-    public static String getDataTime(String format) {
+    public static String getFormatNowTime(String format) {
         SimpleDateFormat df = new SimpleDateFormat(format, Locale.getDefault());
         return df.format(new Date());
-    }
-
-    //字符串转时间戳
-
-    /**
-     * 指定格式字符串转时间戳
-     *
-     * @param timeString 要转时间戳的字符串，格式指定，如"yyyy年MM月dd日 hh:mm"
-     * @return 毫秒值
-     */
-    public static String getTime(String timeString) {
-        String timeStamp = null;
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 hh:mm");
-        Date d;
-        try {
-            d = sdf.parse(timeString);
-            long l = d.getTime();
-            timeStamp = String.valueOf(l);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return timeStamp;
     }
 
     /**
      * 格式化时间
      *
-     * @param time 毫秒值
-     * @return 返回yyyy-MM-dd格式年月日
+     * @param date   Date对象
+     * @param format String 想要格式化的日期形式,如"yyyy年MM月dd日 HH:mm"
+     * @return 返回指定格式返回当前系统时间, 如"XXXX年XX月XX日 XX:XX"
      */
-    public static String getDataTime(long time) {
+    public static String getFormatTime(Date date, String format) {
+        SimpleDateFormat df = new SimpleDateFormat(format, Locale.getDefault());
+        return df.format(date);
+    }
+
+    /**
+     * 格式化时间
+     *
+     * @param time   毫秒值
+     * @param format String 想要格式化的日期形式,如"yyyy年MM月dd日 HH:mm"
+     * @return 返回指定格式返回当前系统时间, 如"XXXX年XX月XX日 XX:XX"
+     */
+    public static String getFormatTime(long time, String format) {
         Date d = new Date(time);
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.getDefault());
         return sdf.format(d);
     }
 
@@ -65,29 +57,78 @@ public class DateUtils {
      * @param newPattern String 想要格式化成什么格式,如"yyyy年MM月dd日"
      * @return 返回格式化完成的字符串
      */
-    public static String StringPattern(String date, String oldPattern, String newPattern) {
-        if (date == null || oldPattern == null || newPattern == null)
+    public static String getFormatTime(String date, String oldPattern, String newPattern) {
+        if (date == null || oldPattern == null || newPattern == null) {
             return "";
-        SimpleDateFormat sdf1 = new SimpleDateFormat(oldPattern);        // 实例化模板对象
-        SimpleDateFormat sdf2 = new SimpleDateFormat(newPattern);        // 实例化模板对象
+        }
+        // 实例化模板对象
+        SimpleDateFormat sdf1 = new SimpleDateFormat(oldPattern);
+        // 实例化模板对象
+        SimpleDateFormat sdf2 = new SimpleDateFormat(newPattern);
         Date d = null;
         try {
-            d = sdf1.parse(date);   // 将给定的字符串中的日期提取出来
-        } catch (Exception e) {            // 如果提供的字符串格式有错误，则进行异常处理
-            e.printStackTrace();       // 打印异常信息
+            // 将给定的字符串中的日期提取出来
+            d = sdf1.parse(date);
+        } catch (Exception e) {
+            // 如果提供的字符串格式有错误，则进行异常处理
+            // 打印异常信息
+            e.printStackTrace();
         }
         return sdf2.format(d);
     }
 
-
     /**
-     * 返回当前系统时间(格式以HH:mm形式)
+     * 指定格式字符串转时间戳
      *
-     * @return 返回当前系统时间时分(格式以HH:mm形式)
+     * @param date 要转时间戳的字符串，格式指定包括"yyyy-MM-dd hh:mm:ss",
+     *             "yyyyMMddHHmmss","yyyyMMdd","yyyy-MM-dd","yyyy-MM-dd  HH:mm"五种形式
+     * @return 毫秒值
      */
-    public static String getDataTime() {
-        return getDataTime("HH:mm");
+    public static long getLongTime(String date) {
+        date = date.trim();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //yyyyMMddHHmmss
+        String a1 = "[0-9]{4}[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}";
+        //yyyyMMdd
+        String a2 = "[0-9]{4}[0-9]{2}[0-9]{2}";
+        //yyyy-MM-dd HH:mm:ss
+        String a3 = "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}";
+        //yyyy-MM-dd
+        String a4 = "[0-9]{4}-[0-9]{2}-[0-9]{2}";
+        //yyyy-MM-dd  HH:mm
+        String a5 = "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}";
+        boolean datea1 = Pattern.compile(a1).matcher(date).matches();
+        if (datea1) {
+            sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        }
+        boolean datea2 = Pattern.compile(a2).matcher(date).matches();
+        if (datea2) {
+            sdf = new SimpleDateFormat("yyyyMMdd");
+        }
+        boolean datea3 = Pattern.compile(a3).matcher(date).matches();
+        if (datea3) {
+            sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }
+        boolean datea4 = Pattern.compile(a4).matcher(date).matches();
+        if (datea4) {
+            sdf = new SimpleDateFormat("yyyy-MM-dd");
+        }
+        boolean datea5 = Pattern.compile(a5).matcher(date).matches();
+        if (datea5) {
+            sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        }
+        long l = 0L;
+        Date d;
+        try {
+            date = date.trim();
+            d = sdf.parse(date);
+            l = d.getTime();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return l;
     }
+
 
     /**
      * 是不是当日
@@ -112,7 +153,22 @@ public class DateUtils {
 
 
     /**
-     * 根据当前日期获得是星期几
+     * 根据传入日期获得是星期几
+     *
+     * @param date Date对象
+     * @return 返回周几
+     */
+    public static String getWeek(Date date) {
+        if (null != date) {
+            String time = getFormatTime(date, "yyyy-MM-dd");
+            return getWeek(time);
+        } else {
+            return "格式错误";
+        }
+    }
+
+    /**
+     * 根据传入日期获得是星期几
      *
      * @param time yyyy-MM-dd格式年月日字符串
      * @return 返回周几
@@ -201,10 +257,14 @@ public class DateUtils {
      */
     public static String getTimeLineOne(Date date) {
         String showStr = "";
-        long yearSeconds = 31536000L;//365 * 24 * 60 * 60;
-        long monthSeconds = 2592000L;//30 * 24 * 60 * 60;
-        long daySeconds = 86400L;//24 * 60 * 60;
-        long hourSeconds = 3600L;//60 * 60;
+        //365 * 24 * 60 * 60;
+        long yearSeconds = 31536000L;
+        //30 * 24 * 60 * 60;
+        long monthSeconds = 2592000L;
+        //24 * 60 * 60;
+        long daySeconds = 86400L;
+        //60 * 60;
+        long hourSeconds = 3600L;
         long minuteSeconds = 60L;
 
         long time = (System.currentTimeMillis() - date.getTime()) / 1000;
@@ -214,9 +274,9 @@ public class DateUtils {
         }
         if (time / yearSeconds > 0) {
             int year = (int) (time / yearSeconds);
-            if (year > 10)
+            if (year > 10) {
                 showStr = "很久以前";
-            else {
+            } else {
                 showStr = year + "年前";
             }
         } else if (time / monthSeconds > 0) {
